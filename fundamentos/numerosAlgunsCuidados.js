7cb0572562c0ca4eb5c81f95f88f1e0a7466637b
Gean-Santos <geansantos_15@hotmail.com>
2019-05-09 03:42:57 -0300

    Primeiro commit

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,8 @@
+console.log(7 / 0) //infinito
+console.log('10' / 2)
+console.log('3' + 2) // Como + é cocatenador, o 3 continua uma string
+console.log('3' - 2)// Nesse caso, 3 é transformado em number
+console.log('Show!' * 2) // not a number
+console.log(0.1 + 0.7) //gera imperfeicões
+//console.log(10.toString())
+console.log((10.345).toFixed(2))
---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,18 @@
+
+var a = 3
+let b = 4
+
+//Var pode ser definida novamente com o mesmo nome, let apenas uma vez com cada nome
+var a = 30
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 5
+//c = 50
+console.log(c)
