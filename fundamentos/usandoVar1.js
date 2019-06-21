774347f1986d34f3a0cdfa6d1a37ba8beee7c39f
Gean-Santos <geansantos_15@hotmail.com>
2019-06-20 21:01:49 -0300

    Meu commit

---
+++ fundamentos/usandoVar1.js
@@ -0,0 +1,19 @@
+{
+    {
+        {
+            {
+                var sera = 'Seráaaaa?'
+                console.log(sera)
+            }
+        }
+    }
+}
+
+console.log(sera)
+
+function teste() {
+    var local = 123
+    console.log(local)
+}
+teste()
+//console.log(local)
